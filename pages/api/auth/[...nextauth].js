1a828f2ccafff0ccd3963463709642e528f6a7a3
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-02-19 14:36:55 +0600

    Add tokens using jwt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,5 +13,26 @@ export default NextAuth({
     secret: process.env.JWT_SECRET,
     pages: {
         signIn: '/login'
-    }
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000,
+                };
+            }
+
+            if (Date.now() < token.accessTokenExpires) {
+                console.log('Existing token is valid!!!');
+                return token;
+            }
+
+            console.log('Access token has expired, refreshing....');
+            return await refreshAccessToken(token);
+        },
+    },
 });
